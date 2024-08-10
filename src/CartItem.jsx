47e8bbe3d9f13b4c66c256d46ad977cff6c229fd
manasamohandoss <manasa.mohandoss@accenture.com>
2024-08-10 13:39:29 -0400

    4th Commit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -21,7 +21,7 @@ const CartItem = ({ onContinueShopping }) => {
 };
 
   const handleContinueShopping = (e) => {
-   onContinueShopping();
+   onContinueShopping(e);
   };
 
   const handleIncrement = (item) => {
@@ -37,7 +37,7 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleRemove = (item) => {
-    dispatch(removeItem ({name: item.name}));
+    dispatch(removeItem (item.name));
   };
 
  // Calculate total cost based on quantity for an item
@@ -70,7 +70,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)} >Checkout</button>
       </div>
     </div>
   );
