3a85df3ba82e338921efece8004e864a6d2ec098
manasamohandoss <manasa.mohandoss@accenture.com>
2024-08-10 07:45:21 -0400

    commit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice';
+import { addItem, removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
@@ -10,39 +10,41 @@ const CartItem = ({ onContinueShopping }) => {
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
     let totalAmount = 0;
-    cart.forEach(item => {
-      totalAmount += parseFloat(item.cost.replace('$', ''))  * item.quantity;
-    });
-    return totalAmount; 
+    cart.map((plant)=>{
+      totalAmount+=parseInt(plant.cost.slice(1))*plant.quantity;
+    })                                             
+    return totalAmount;
   };
+
+  const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+};
 
   const handleContinueShopping = (e) => {
    onContinueShopping();
   };
 
   const handleIncrement = (item) => {
-    dispatch(updateQuantity({ name: item.name, quantity: item.quantity +1}));
+    dispatch(addItem(item));
   };
 
   const handleDecrement = (item) => {
-    if(item.quantity > 1) {
-    dispatch(updateQuantity({ name: item.name, quantity: item.quantity -1}));
-    }else {
-      dispatch(removeItem (item));
-    }   
+    if(item.quantity===1){
+      dispatch(removeItem(item.name));
+    } else{
+      dispatch(updateQuantity({name:item.name,quantity:item.quantity-1}));
+    }
   };
 
   const handleRemove = (item) => {
     dispatch(removeItem ({name: item.name}));
   };
 
-  // Calculate total cost based on quantity for an item
+ // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
-    return parseFloat(item.cost.replace('$', '')) * item.quantity;
+    return item.quantity*parseInt(item.cost.slice(1));
   };
-const handleCheckoutShopping = (e) => {
-  alert('Functionality to be added for future reference');
-};
+
   return (
     <div className="cart-container">
       <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
